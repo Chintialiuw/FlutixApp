c85434ce8064d1b92f966b242f49f0e7dc9abeac
Aliya irfani <aliyairfani28@gmail.com>
2023-10-25 19:06:16 +0800

    Update ticket.dart

--- lib/home/ticket.dart
+++ lib/home/ticket.dart
@@ -10,6 +10,33 @@ class TicketPage extends StatefulWidget {
 class _TicketPageState extends State<TicketPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: ListView(
+        children: [
+          Text(
+            "My Tickets",
+            style: TextStyle(fontSize: 24),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 26, 18, 11),
+                    foregroundColor: Color.fromARGB(255, 229, 229, 203),
+                  ),
+                  child: Text("New")),
+              ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 26, 18, 11),
+                    foregroundColor: Color.fromARGB(255, 229, 229, 203),
+                  ),
+                  child: Text("Used")),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
