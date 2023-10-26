d98a2790c73cf6389d3dc8ee296dde66b318a523
Aliya irfani <aliyairfani28@gmail.com>
2023-10-26 09:59:43 +0800

    menambahkan foto profil

--- lib/home/profile.dart
+++ lib/home/profile.dart
@@ -10,6 +10,34 @@ class ProfilPage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(25),
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 50,
+                      backgroundImage: AssetImage("images/card/minji.jpg"),
+                    ),
+                    Text(
+                      "Chintia Liu Wintin",
+                      style: TextStyle(fontSize: 24),
+                    ),
+                    Text(
+                      "chintialiuw@gmail.com",
+                      style: TextStyle(fontSize: 16),
+                    )
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
