e23d7375999f04f62844e1cb9ecbce78539f46b1
Sherina Laraswati <93170060+SherinaLaraswati@users.noreply.github.com>
2023-10-29 00:31:44 +0800

    sign-in 2

--- lib/ui/pages/splash_screen/sign_in.dart
+++ lib/ui/pages/splash_screen/sign_in.dart
@@ -12,38 +12,43 @@ class _signInState extends State<signIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      body: ListView(
+        scrollDirection: Axis.vertical,
         children: [
-          Row(
+          Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Padding(
-                padding: const EdgeInsets.only(
-                  top: 60, left:20
-                ),
-                child: Text("Welcome back,",
-                style: TextStyle(
-                  fontSize: 26,
-                  color: Colors.black,
-                ),
-                ),
-              ),
-              
-              Padding(
-                padding: const EdgeInsets.only(
-                   top: 70, left:85
-                ),
-                child: Container(
-                  width: 80,
-                  height: 80,
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 255, 255, 255),
-                    image: DecorationImage(
-                      image: AssetImage("assets/splash/logo-black.png"),
-                      fit: BoxFit.cover,
-                      )
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 60, left:20
+                    ),
+                    child: Text("Welcome back,",
+                    style: TextStyle(
+                      fontSize: 26,
+                      color: Colors.black,
+                    ),
+                    ),
                   ),
-                ),
+                  
+                  Padding(
+                    padding: const EdgeInsets.only(
+                       top: 70, left:85
+                    ),
+                    child: Container(
+                      width: 80,
+                      height: 80,
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 255, 255, 255),
+                        image: DecorationImage(
+                          image: AssetImage("assets/splash/logo-black.png"),
+                          fit: BoxFit.cover,
+                          )
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
@@ -57,6 +62,83 @@ class _signInState extends State<signIn> {
               color: Colors.black,
             ),
             ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 80,left: 20,right: 20
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderSide: BorderSide()
+                ),
+                labelText: "Email Address",
+
+                labelStyle: TextStyle(
+                  color: Colors.black
+                ),
+
+                hintText: "chintialiuw@gmail.com",
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey,
+                    width: 3,
+                  ),
+                ),
+                
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color.fromARGB(255, 170, 145, 31),
+                    width: 3,
+                    ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 40,left: 20,right: 20
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderSide: BorderSide()
+                ),
+                labelText: "Password",
+
+                labelStyle: TextStyle(
+                  color: Colors.black
+                ),
+
+                hintText: "*********",
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey,
+                    width: 3,
+                  ),
+                ),
+                
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color.fromARGB(255, 170, 145, 31),
+                    width: 3,
+                    ),
+                ),
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 90, left: 30),
+                child: Text("Continue to Sign In",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
