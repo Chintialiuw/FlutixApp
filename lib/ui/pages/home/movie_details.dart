98393c440a2f108c248eb7940f4be6178fb1b780
Chintialiuw <chintialiu186@gmail.com>
2023-10-29 09:57:19 +0800

    Update : movie_details (belum ada cast)

--- lib/ui/pages/home/movie_details.dart
+++ lib/ui/pages/home/movie_details.dart
@@ -16,174 +16,181 @@ class _movie_details_State extends State<movie_details> {
   Widget build(BuildContext context) {
     var lebar = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        leading: Padding(
-          padding: const EdgeInsets.only(top: 10.0),
-          child: InkWell(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) {
-                    return HomePage();
-                  },
-                ),
-              );
-            },
-            child: Icon(
-              CupertinoIcons.back,
-              color: Colors.black,
-            ),
-          ),
-        ),
-      ),
-      body: ListView(
-        children: [
-          Column(
-            children: [
-              Container(
-                width: lebar,
-                height: 300,
-                color: Colors.grey,
-              ),
-              SizedBox(height: 20),
-              Text(
-                "Monster Inc",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 3),
-              Text(
-                "Adventure - Indonesian",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 14,
-                  fontWeight: FontWeight.normal,
-                ),
-              ),
-              SizedBox(height: 3),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  for (int i = 0; i < 4; i++)
-                    Icon(
-                      Icons.star_rounded,
-                      color: Colors.black,
-                      size: 16,
+      body: SafeArea(
+        child: ListView(
+          children: [
+            Column(
+              children: [
+                Container(
+                  width: lebar,
+                  height: 300,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("images/card/monsInc.png"),
+                      fit: BoxFit.cover,
                     ),
-                  Icon(
-                    Icons.star_half_rounded,
-                    color: Colors.black,
-                    size: 16,
                   ),
-                  Text(
-                    "  8 / 10",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 12,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20.0),
-                    child: Text(
-                      "StoryLine",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18,
-                        fontWeight: FontWeight.normal,
+                  child: Stack(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20.0, left: 20.0),
+                        child: Align(
+                          alignment: Alignment.topLeft,
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.pop(context);
+                            },
+                            child: Icon(
+                              CupertinoIcons.back,
+                              color: Colors.white,
+                              size: 32,
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
-              ),
-              SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-                child: Text(
-                  "Sulli, raksasa dari perusahaan listrik menakutkan yang menemukan kasih sayang dalam diri Boo, seorang anak berusia dua tahun.",
-                  textAlign: TextAlign.left,
+                ),
+                SizedBox(height: 20),
+                Text(
+                  "Monster Inc",
                   style: TextStyle(
-                    fontSize: 16,
+                    color: Colors.black,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 3),
+                Text(
+                  "Adventure - Indonesian",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
                     fontWeight: FontWeight.normal,
                   ),
                 ),
-              ),
-              SizedBox(height: 20),
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20.0),
-                    child: Text(
-                      "Cast",
+                SizedBox(height: 3),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    for (int i = 0; i < 4; i++)
+                      Icon(
+                        Icons.star_rounded,
+                        color: Colors.black,
+                        size: 16,
+                      ),
+                    Icon(
+                      Icons.star_half_rounded,
+                      color: Colors.black,
+                      size: 16,
+                    ),
+                    Text(
+                      "  8 / 10",
                       style: TextStyle(
                         color: Colors.black,
-                        fontSize: 18,
-                        fontWeight: FontWeight.normal,
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: Text(
+                        "StoryLine",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontWeight: FontWeight.normal,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  for (int i = 0; i < 5; i++)
-                    Container(
-                      width: 80,
-                      height: 80,
-                      margin: EdgeInsets.only(left: 20.0, top: 10.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5),
-                        color: Colors.grey,
-                      ),
-                    ),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.all(50.0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return placesdate();
-                        },
-                      ),
-                    );
-                  },
+                  ],
+                ),
+                SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0, right: 20.0),
                   child: Text(
-                    "Continue to Book",
+                    "Sulli, raksasa dari perusahaan listrik menakutkan yang menemukan kasih sayang dalam diri Boo, seorang anak berusia dua tahun.",
+                    textAlign: TextAlign.left,
                     style: TextStyle(
                       fontSize: 16,
-                      fontWeight: FontWeight.bold,
+                      fontWeight: FontWeight.normal,
                     ),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.orange,
-                    foregroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    minimumSize: Size(200, 50),
                   ),
                 ),
-              ),
-            ],
-          ),
-        ],
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: Text(
+                        "Cast",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      for (int i = 0; i < 6; i++)
+                        Container(
+                          width: 80,
+                          height: 80,
+                          margin: EdgeInsets.only(left: 20.0, top: 10.0),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            color: Colors.grey,
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(50.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return placesdate();
+                          },
+                        ),
+                      );
+                    },
+                    child: Text(
+                      "Continue to Book",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orange,
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      minimumSize: Size(200, 50),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
