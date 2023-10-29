b9126c005f0846b0e9c1e71fb861bc660ed843d0
Aliya irfani <aliyairfani28@gmail.com>
2023-10-29 11:57:27 +0800

    add page detailTicket

---
+++ lib/ui/pages/home/detailTicket.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class detailTicket extends StatefulWidget {
+  const detailTicket({super.key});
+
+  @override
+  State<detailTicket> createState() => _detailTicketState();
+}
+
+class _detailTicketState extends State<detailTicket> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/ui/pages/home/ticket.dart
+++ lib/ui/pages/home/ticket.dart
@@ -1,3 +1,4 @@
+import 'package:flutixapp/ui/pages/home/detailTicket.dart';
 import 'package:flutter/material.dart';
 
 class TicketPage extends StatefulWidget {
@@ -17,135 +18,148 @@ class _TicketPageState extends State<TicketPage> {
             padding: const EdgeInsets.only(
               top: 20,
             ),
-            child: Column(
-              children: [
-                Text(
-                  "My Tickets",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: Color.fromARGB(255, 26, 18, 11),
-                            foregroundColor: Color.fromARGB(255, 229, 229, 203),
-                            fixedSize: Size(120, 20)),
-                        child: Text("New"),
+            child: Container(
+              width: MediaQuery.of(context).size.width - 90,
+              child: Column(
+                children: [
+                  Text(
+                    "My Tickets",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Color.fromARGB(255, 26, 18, 11),
+                              foregroundColor:
+                                  Color.fromARGB(255, 229, 229, 203),
+                              fixedSize: Size(120, 20)),
+                          child: Text("New"),
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: Color.fromARGB(255, 26, 18, 11),
-                            foregroundColor: Color.fromARGB(255, 229, 229, 203),
-                            fixedSize: Size(120, 20)),
-                        child: Text("Used"),
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Color.fromARGB(255, 26, 18, 11),
+                              foregroundColor:
+                                  Color.fromARGB(255, 229, 229, 203),
+                              fixedSize: Size(120, 20)),
+                          child: Text("Used"),
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  children: [
-                    InkWell(
-                      onTap: () {},
-                      child: Container(
-                        width: 75,
-                        height: 100,
-                        margin: const EdgeInsets.only(left: 20),
-                        child: Image.asset("images/card/Carl's-date.png",
-                            fit: BoxFit.fill),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      InkWell(
+                        onTap: () {},
+                        child: Container(
+                          width: 75,
+                          height: 100,
+                          margin: const EdgeInsets.only(left: 20),
+                          child: Image.asset("images/card/Carl's-date.png",
+                              fit: BoxFit.fill),
+                        ),
                       ),
-                    ),
-                    TextButton(onPressed: () {}, child: Text("Carl's Date"))
-                  ],
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  children: [
-                    InkWell(
-                      onTap: () {},
-                      child: Container(
-                        width: 75,
-                        height: 100,
-                        margin: const EdgeInsets.only(left: 20, bottom: 20),
-                        child: Image.asset("images/card/Carl's-date.png",
-                            fit: BoxFit.fill),
+                      TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => detailTicket()),
+                            );
+                          },
+                          child: Text("Carl's Date"))
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      InkWell(
+                        onTap: () {},
+                        child: Container(
+                          width: 75,
+                          height: 100,
+                          margin: const EdgeInsets.only(left: 20, bottom: 20),
+                          child: Image.asset("images/card/Carl's-date.png",
+                              fit: BoxFit.fill),
+                        ),
                       ),
-                    ),
-                    TextButton(onPressed: () {}, child: Text("Carl's Date"))
-                  ],
-                ),
-                Row(
-                  children: [
-                    InkWell(
-                      onTap: () {},
-                      child: Container(
-                        width: 75,
-                        height: 100,
-                        margin: const EdgeInsets.only(left: 20, bottom: 20),
-                        child: Image.asset("images/card/Carl's-date.png",
-                            fit: BoxFit.fill),
+                      TextButton(onPressed: () {}, child: Text("Carl's Date"))
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      InkWell(
+                        onTap: () {},
+                        child: Container(
+                          width: 75,
+                          height: 100,
+                          margin: const EdgeInsets.only(left: 20, bottom: 20),
+                          child: Image.asset("images/card/Carl's-date.png",
+                              fit: BoxFit.fill),
+                        ),
                       ),
-                    ),
-                    TextButton(onPressed: () {}, child: Text("Carl's Date"))
-                  ],
-                ),
-                Row(
-                  children: [
-                    InkWell(
-                      onTap: () {},
-                      child: Container(
-                        width: 75,
-                        height: 100,
-                        margin: const EdgeInsets.only(left: 20, bottom: 20),
-                        child: Image.asset("images/card/Carl's-date.png",
-                            fit: BoxFit.fill),
+                      TextButton(onPressed: () {}, child: Text("Carl's Date"))
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      InkWell(
+                        onTap: () {},
+                        child: Container(
+                          width: 75,
+                          height: 100,
+                          margin: const EdgeInsets.only(left: 20, bottom: 20),
+                          child: Image.asset("images/card/Carl's-date.png",
+                              fit: BoxFit.fill),
+                        ),
                       ),
-                    ),
-                    TextButton(onPressed: () {}, child: Text("Carl's Date"))
-                  ],
-                ),
-                Row(
-                  children: [
-                    InkWell(
-                      onTap: () {},
-                      child: Container(
-                        width: 75,
-                        height: 100,
-                        margin: const EdgeInsets.only(left: 20, bottom: 20),
-                        child: Image.asset("images/card/Carl's-date.png",
-                            fit: BoxFit.fill),
+                      TextButton(onPressed: () {}, child: Text("Carl's Date"))
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      InkWell(
+                        onTap: () {},
+                        child: Container(
+                          width: 75,
+                          height: 100,
+                          margin: const EdgeInsets.only(left: 20, bottom: 20),
+                          child: Image.asset("images/card/Carl's-date.png",
+                              fit: BoxFit.fill),
+                        ),
                       ),
-                    ),
-                    TextButton(onPressed: () {}, child: Text("Carl's Date"))
-                  ],
-                ),
-                Row(
-                  children: [
-                    InkWell(
-                      onTap: () {},
-                      child: Container(
-                        width: 75,
-                        height: 100,
-                        margin: const EdgeInsets.only(left: 20, bottom: 20),
-                        child: Image.asset("images/card/Carl's-date.png",
-                            fit: BoxFit.fill),
+                      TextButton(onPressed: () {}, child: Text("Carl's Date"))
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      InkWell(
+                        onTap: () {},
+                        child: Container(
+                          width: 75,
+                          height: 100,
+                          margin: const EdgeInsets.only(left: 20, bottom: 20),
+                          child: Image.asset("images/card/Carl's-date.png",
+                              fit: BoxFit.fill),
+                        ),
                       ),
-                    ),
-                    TextButton(onPressed: () {}, child: Text("Carl's Date"))
-                  ],
-                )
-              ],
+                      TextButton(onPressed: () {}, child: Text("Carl's Date"))
+                    ],
+                  )
+                ],
+              ),
             ),
           )
         ],
