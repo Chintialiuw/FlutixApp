fe2cad64e6deece96ae4f4ccadbef5dbdf209f44
Chintialiuw <chintialiu186@gmail.com>
2023-10-29 08:54:05 +0800

    addFile : success_checkout

---
+++ lib/ui/pages/home/success_checkout.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class success_checkout extends StatefulWidget {
+  success_checkout({super.key});
+
+  @override
+  State<success_checkout> createState() => _success_checkout_State();
+}
+
+class _success_checkout_State extends State<success_checkout> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
