45d870d659303e079b3b0734ef439af5162a182d
Chintialiuw <chintialiu186@gmail.com>
2023-11-20 08:36:29 +0800

    Update confirmation.dart

--- lib/ui/pages/splash_screen/confirmation.dart
+++ lib/ui/pages/splash_screen/confirmation.dart
@@ -1,6 +1,7 @@
 import 'package:flutixapp/ui/pages/splash_screen/sign_in.dart';
 import 'package:flutixapp/ui/pages/splash_screen/user_profile.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class confir extends StatefulWidget {
@@ -33,86 +34,98 @@ class _confirState extends State<confir> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: Padding(
+          padding: const EdgeInsets.only(top: 10.0, left: 20),
+          child: InkWell(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Icon(
+              Icons.arrow_back_ios,
+              color: Color(0xFFE1A20B),
+              size: 32,
+            ),
+          ),
+        ),
+      ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          GestureDetector(
-            onTap: () {
-              Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (context) => signIn()));
-            },
-            child: Padding(
-              padding: const EdgeInsets.only(top: 50, left: 30),
-              child: Icon(
-                Icons.arrow_back_ios,
-                color: Colors.yellow,
-                size: 40,
-              ),
-            ),
-          ),
           Padding(
-            padding: const EdgeInsets.only(top: 15, left: 30),
+            padding: const EdgeInsets.only(top: 15, left: 20),
             child: Text(
               "Confirm",
-              style: TextStyle(
-                fontSize: 24,
-                color: Colors.black,
-              ),
+              style: GoogleFonts.raleway(
+                  fontSize: 24,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold),
             ),
           ),
           Padding(
-            padding: const EdgeInsets.only(top: 10, left: 30),
+            padding: const EdgeInsets.only(top: 10, left: 20),
             child: Text(
               "New Account",
-              style: TextStyle(
-                fontSize: 24,
-                color: Colors.black,
-              ),
+              style: GoogleFonts.raleway(
+                  fontSize: 24,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 140, top: 110),
-            child: Container(
-              width: 130,
-              height: 130,
-              decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 20, 16, 16),
-                  borderRadius: BorderRadius.circular(90),
-                  image: DecorationImage(
-                    image: AssetImage("assets/images/card/minji.jpg"),
-                    fit: BoxFit.cover,
-                  )),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 10, left: 160),
-            child: Text(
-              "Welcome,",
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.black,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 10, left: 120),
-            child: Text(
-              username ?? "Loading...",
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.black,
-              ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 90.0),
+                  child: Container(
+                    width: 130,
+                    height: 130,
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 20, 16, 16),
+                      borderRadius: BorderRadius.circular(90),
+                      image: DecorationImage(
+                        image: AssetImage("assets/images/card/minji.jpg"),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 30),
+                  child: Text(
+                    "Welcome,",
+                    style: GoogleFonts.raleway(
+                      fontSize: 20,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Text(
+                    username ?? "Loading...",
+                    style: GoogleFonts.raleway(
+                      fontSize: 20,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
           Row(
             children: [
               Padding(
-                padding: const EdgeInsets.only(top: 300, left: 30),
+                padding: const EdgeInsets.only(top: 80, left: 20, bottom: 40),
                 child: Text(
-                  "Yes. I am In",
-                  style: TextStyle(
-                    fontSize: 20,
+                  "Yes, I am In",
+                  style: GoogleFonts.raleway(
                     color: Colors.black,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 20,
                   ),
                 ),
               ),
@@ -122,10 +135,10 @@ class _confirState extends State<confir> {
                       .push(MaterialPageRoute(builder: (context) => uprof()));
                 },
                 child: Padding(
-                  padding: const EdgeInsets.only(left: 170, top: 300),
+                  padding: const EdgeInsets.only(left: 155, right: 20, top: 100, bottom: 40),
                   child: Icon(
                     Icons.arrow_circle_right,
-                    color: Colors.yellow,
+                    color: Color(0xFFE1A20B),
                     size: 60,
                   ),
                 ),
