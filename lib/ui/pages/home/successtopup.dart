123595fb99127e91796ac7902bee3916cee0d64e
Chintialiuw <chintialiu186@gmail.com>
2023-11-20 09:39:09 +0800

    Update successtopup.dart

--- lib/ui/pages/home/successtopup.dart
+++ lib/ui/pages/home/successtopup.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: camel_case_types, prefer_const_constructors
 
+import 'package:flutixapp/ui/pages/home/myWallet.dart';
+import 'package:flutixapp/ui/widgets/bottomnav.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class successtopup extends StatefulWidget {
   const successtopup({super.key});
@@ -13,48 +16,96 @@ class _successtopupState extends State<successtopup> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Padding(
-            padding: EdgeInsets.only(top: 100, bottom: 50),
-            child: Image.asset("images/money.png"),
-          ),
-          Text(
-            "Wallet is Ready!",
-            style: TextStyle(
-                color: Colors.black, fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Text("You have successfully",
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                  fontWeight: FontWeight.normal)),
-          Text("feed your wallet",
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                  fontWeight: FontWeight.normal)),
-          Padding(padding: EdgeInsets.only(top: 100)),
-          ElevatedButton(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(
-                backgroundColor: Color.fromARGB(255, 191, 203, 20),
-                foregroundColor: Color.fromARGB(255, 229, 229, 203),
-                fixedSize: Size(180, 20)),
-            child: Text(
-              "My Wallet",
-              style: TextStyle(color: Colors.white),
+      body: SafeArea(
+        child: ListView(
+          children: [
+            Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 60.0, left: 10.0, right: 10.0),
+                    child: Container(
+                      width: 250,
+                      height: 300,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(
+                              "assets/images/card/money.png"),
+                          fit: BoxFit.fitHeight,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    "Wallet is Ready !",
+                    style: GoogleFonts.raleway(
+                        color: Colors.black,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "You have succesfully feed your wallet",
+                    style: GoogleFonts.raleway(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.normal),
+                  ),
+                  SizedBox(height: 80),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return myWallet();
+                          },
+                        ),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFFE1A20B),
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      minimumSize: Size(200, 50),
+                    ),
+                    child: Text(
+                      "My Wallet",
+                      style: GoogleFonts.raleway(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return BottomNav();
+                          },
+                        ),
+                      );
+                    },
+                    child: Text(
+                      "Back to Home",
+                      style: GoogleFonts.raleway(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text("Back to Home", style: TextStyle(color: Color(0xFFAEAEAE))),
-        ],
+          ],
+        ),
       ),
     );
   }
