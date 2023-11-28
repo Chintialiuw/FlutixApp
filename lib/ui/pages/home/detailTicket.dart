015e0a2d04c3fba3e5c27db58b22dc676ea7149a
Aliya irfani <aliyairfani28@gmail.com>
2023-11-28 20:21:23 +0800

    rapikan detailticket(blm final)

--- lib/ui/pages/home/detailTicket.dart
+++ lib/ui/pages/home/detailTicket.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: camel_case_types
+// ignore_for_file: camel_case_types, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutixapp/ui/pages/home/ticket.dart';
 import 'package:flutter/material.dart';
@@ -18,139 +18,177 @@ class _detailTicketState extends State<detailTicket> {
         children: [
           Column(
             children: [
-              Row(
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => TicketPage()));
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 20, left: 20),
-                      child: Icon(
-                        Icons.arrow_back_ios,
-                        color: Colors.yellow,
-                        size: 40,
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => TicketPage()));
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 20, left: 20),
+                            child: Icon(
+                              Icons.arrow_back_ios,
+                              color: Colors.yellow,
+                              size: 40,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          "Ticket",
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        width: 276,
+                        height: 131,
+                        margin: const EdgeInsets.only(top: 20, left: 20),
+                        child: Image.asset("assets/images/card/monster.png",
+                            fit: BoxFit.fill),
                       ),
                     ),
-                  ),
-                  Text(
-                    "Ticket",
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-              InkWell(
-                onTap: () {},
-                child: Container(
-                  width: 276,
-                  height: 131,
-                  margin: const EdgeInsets.only(top: 20, left: 20),
-                  child: Image.asset("assets/images/card/monster.png",
-                      fit: BoxFit.fill),
+                    Text("Monster, Inc.",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.normal)),
+                    Text("Adventure - Indonesia",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 12,
+                            fontWeight: FontWeight.normal)),
+                    Row(
+                      children: List.generate(5, (index) {
+                        return Icon(
+                          Icons.star,
+                          color: Colors.yellow,
+                          size: 12,
+                        );
+                      }),
+                    ),
+                    SizedBox(height: 15),
+                    Row(
+                      children: [
+                        Text("Cinema",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 14,
+                                fontWeight: FontWeight.normal)),
+                        Spacer(),
+                        Text("Paris Van Java Mall",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 14,
+                                fontWeight: FontWeight.normal)),
+                      ],
+                    ),
+                    SizedBox(height: 15),
+                    Row(
+                      children: [
+                        Text("Date & Time",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 14,
+                                fontWeight: FontWeight.normal)),
+                        Spacer(),
+                        Text("280524 & 20:00",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 14,
+                                fontWeight: FontWeight.normal)),
+                      ],
+                    ),
+                    SizedBox(height: 15),
+                    Row(
+                      children: [
+                        Text("Seat",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 14,
+                                fontWeight: FontWeight.normal)),
+                        Spacer(),
+                        Text("C1, C2",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 14,
+                                fontWeight: FontWeight.normal)),
+                      ],
+                    ),
+                    SizedBox(height: 15),
+                    Row(
+                      children: [
+                        Column(
+                          children: [
+                            Column(
+                              children: [
+                                Text("Name",
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.normal)),
+                                // Spacer(),
+                                Text("(nama)",
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.normal)),
+                              ],
+                            ),
+                            SizedBox(height: 15),
+                            Column(
+                              children: [
+                                Text("Price",
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.normal)),
+                                // Spacer(),
+                                Text("Rp",
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.normal)),
+                              ],
+                            ),
+                          ],
+                        ),
+                        Spacer(),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 5),
+                          child: CircleAvatar(
+                            radius: 50,
+                            // backgroundImage: Image.network(user.foto).image,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 15),
+                    Row(
+                      children: [
+                        Text("ID Order",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 14,
+                                fontWeight: FontWeight.normal)),
+                        Spacer(),
+                        Text("2208199612389",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 14,
+                                fontWeight: FontWeight.normal)),
+                      ],
+                    )
+                  ],
                 ),
               ),
-              Text("Monster, Inc.",
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 16,
-                      fontWeight: FontWeight.normal)),
-              Text("Adventure - Indonesia",
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 12,
-                      fontWeight: FontWeight.normal)),
-              Row(
-                children: List.generate(5, (index) {
-                  return Icon(
-                    Icons.star,
-                    color: Colors.yellow,
-                    size: 12,
-                  );
-                }),
-              ),
-              Row(
-                children: [
-                  Text("Cinema",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 14,
-                          fontWeight: FontWeight.normal)),
-                  SizedBox(
-                    width: 285,
-                  ),
-                  Text("Paris Van Java Mall",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 14,
-                          fontWeight: FontWeight.normal)),
-                ],
-              ),
-              Row(
-                children: [
-                  Text("Date & Time",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 14,
-                          fontWeight: FontWeight.normal)),
-                  SizedBox(
-                    width: 200,
-                  ),
-                  Text("280524 & 20:00",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 14,
-                          fontWeight: FontWeight.normal)),
-                ],
-              ),
-              Row(
-                children: [
-                  Text("Seat",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 14,
-                          fontWeight: FontWeight.normal)),
-                  SizedBox(
-                    width: 285,
-                  ),
-                  Text("C1, C2",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 14,
-                          fontWeight: FontWeight.normal)),
-                ],
-              ),
-              Row(
-                children: [
-                  Text("Name",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 14,
-                          fontWeight: FontWeight.normal)),
-                  Text("Price",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 14,
-                          fontWeight: FontWeight.normal)),
-                ],
-              ),
-              Row(
-                children: [
-                  Text("ID Order",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 14,
-                          fontWeight: FontWeight.normal)),
-                  SizedBox(
-                    width: 285,
-                  ),
-                  Text("2208199612389",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 14,
-                          fontWeight: FontWeight.normal)),
-                ],
-              )
             ],
           )
         ],
